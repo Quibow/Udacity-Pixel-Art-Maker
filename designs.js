8ba8b60673ecbd44181f20a35b4018a665500027
Millie Lin <linxinmin@gmail.com>
2017-10-01 13:36:31 +0800

    Add toggle color display on click event

--- designs.js
+++ designs.js
@@ -1,36 +1,36 @@
 $(function () {
 
 // Select color input
-    let colorPicker = $("#colorPicker");
+    let colorPicker = $('#colorPicker');
 
 // Select size input
-    let heightInput = $("#input_height");
-    let widthInput = $("#input_width");
+    let heightInput = $('#input_height');
+    let widthInput = $('#input_width');
 
 // select canvas
-    const canvas = $("#pixel_canvas");
+    const canvas = $('#pixel_canvas');
 
 // When size is submitted by the user, call makeGrid()
     function makeGrid() {
-        canvas.find("tbody").remove();
+        canvas.find('tbody').remove();
 
         // "submit" the size form to update the grid size
         let gridRows = heightInput.val();
         let gridCol = widthInput.val();
 
         // set tbody to the table
-        canvas.append("<tbody></tbody>");
+        canvas.append('<tbody></tbody>');
 
-        let canvasBody = canvas.find("tbody");
+        let canvasBody = canvas.find('tbody');
 
         // draw grid row
         for (let i = 0; i < gridRows; i++) {
-            canvasBody.append("<tr></tr>");
+            canvasBody.append('<tr></tr>');
         }
 
         // draw grid col
         for (let i = 0; i < gridCol; i++) {
-            canvas.find("tr").append("<td></td>");
+            canvas.find('tr').append('<td class="transparent"></td>');
         }
 
     }
@@ -38,16 +38,25 @@ $(function () {
 
     $(document).ready(function () {
         // click the submit button to update the grid
-        $('input[type="submit"]').on("click", function (e) {
+        $('input[type="submit"]').on('click', function (e) {
             e.preventDefault();
             makeGrid();
         });
 
 
         // toggle grid color
-        $("#pixel_canvas").on("click", "td", function (e) {
+        $('#pixel_canvas').on('click', 'td', function (e) {
             let color = colorPicker.val();
-            $(this).css("background-color", color);
+            let currentColor = $(this).css('background-color');
+            console.log(currentColor+ " color:" + color);
+
+            if($(this).hasClass('transparent')){
+                $(this).toggleClass('transparent');
+                $(this).css("background-color", color);
+            } else{
+                $(this).toggleClass('transparent');
+                $(this).css("background-color", "transparent");
+            }
 
         })
 
