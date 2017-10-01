f2b78a3d30e0af2cfd52a2060617a9ccd7563221
Millie Lin <linxinmin@gmail.com>
2017-10-01 12:56:03 +0800

    add makeGrid function

--- designs.js
+++ designs.js
@@ -1,10 +1,48 @@
+$(function () {
+
 // Select color input
+    let colorPicker = $("#colorPicker");
+
 // Select size input
+    let heightInput = $("#input_height");
+    let widthInput = $("#input_width");
+
+// select canvas
+    const canvas = $("#pixel_canvas");
 
 // When size is submitted by the user, call makeGrid()
+    function makeGrid() {
+        canvas.find("tbody").remove();
 
-function makeGrid() {
+        // "submit" the size form to update the grid size
+        let gridRows = heightInput.val();
+        let gridCol = widthInput.val();
 
-// Your code goes here!
+        // set tbody to the table
+        canvas.append("<tbody></tbody>");
 
-}
+        let canvasBody = canvas.find("tbody");
+
+        // draw grid row
+        for (let i = 0; i < gridRows; i++) {
+            canvasBody.append("<tr></tr>");
+        }
+
+        // draw grid col
+        for (let i = 0; i < gridCol; i++) {
+            canvas.find("tr").append("<td></td>");
+        }
+
+    }
+
+
+    $(document).ready(function () {
+        // click the submit button to update the grid
+        $('input[type="submit"]').on("click", function (e) {
+            e.preventDefault();
+            makeGrid();
+        });
+
+    });
+
+});
